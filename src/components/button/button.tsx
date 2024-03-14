3b27885d2ce56833ae423ac75159bfe47e00764e
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-03-14 14:54:06 +0100

    style: :lipstick: button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,27 +1,40 @@
-import { component$, useStylesScoped$, Slot } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
 
 export interface ButtonProps {
   size?: "small" | "medium" | "large";
+  label?: string;
 }
-export const Button = component$<ButtonProps>(({ size = "medium" }) => {
+
+export const Button = component$<ButtonProps>(({ size = "medium", label = "Button"}) => {
   useStylesScoped$(`
     .size-small {
-      font-size: 10px;
+      font-size: 16px;
     }
     .size-medium {
-      font-size: 14px;
+      font-size: 20px;
     }
     .size-large {
-      font-size: 18px;
+      font-size: 32px;
+    }
+    button {
+      padding: 12px 32px;
+      background-color: #3E6680;
+      color: white;
+      border: none;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      border-radius: 10px;
     }
   `);
+
   return (
     <button
       class={{
         [`size-${size}`]: true,
       }}
     >
-      <Slot></Slot>
+      {label}
     </button>
   );
 });
