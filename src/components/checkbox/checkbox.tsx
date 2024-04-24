3bd7d96ec82d332d4f09fe5c12edb4f6d744899e
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-04-24 21:28:40 +0200

    feat: :bug: make checkbox properties nullable

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -3,8 +3,8 @@ import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import styles from './checkbox.css?inline'
 
 export interface CheckboxProps {
-    checkedSignal: Signal<boolean>;
-    label: string;
+    checkedSignal?: Signal<boolean>;
+    label?: string;
 }
 
 export const Checkbox = component$<CheckboxProps>((props) => {
