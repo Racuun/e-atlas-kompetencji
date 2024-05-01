f884005de5f75201489be7053bfe9753af6e8985
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-05-01 19:15:35 +0200

    style: :lipstick: update scale - remove Signal input

--- web/src/components/scale/scale.tsx
+++ web/src/components/scale/scale.tsx
@@ -1,10 +1,8 @@
-import type { Signal} from '@builder.io/qwik';
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import styles from './scale.css?inline'
 
 export interface ScaleProps {
   id: string,
-  valueSignal: Signal<number>
 }
 
 export const Scale = component$<ScaleProps>((props) => {
@@ -13,27 +11,33 @@ export const Scale = component$<ScaleProps>((props) => {
   return (
     <div class='scale'>
       <label class='container'>
-        <input class='input' type='radio' name={props.id} value={-2} onChange$={() => props.valueSignal.value = -2}/>
+        <input class='input' type='radio' name={props.id} value={-3} />
         <span class='checkmark' />
       </label>
       <label class='container'>
-        <input class='input' type='radio' name={props.id} onChange$={() => props.valueSignal.value = -1}/>
+        <input class='input' type='radio' name={props.id} value={-2} />
         <span class='checkmark' />
       </label>
       <label class='container'>
-        <input class='input' type='radio' name={props.id} onChange$={() => props.valueSignal.value = 0} checked={true}/>
+        <input class='input' type='radio' name={props.id} value={-1} />
         <span class='checkmark' />
       </label>
       <label class='container'>
-        <input class='input' type='radio' name={props.id} onChange$={() => props.valueSignal.value = 1}/>
+        <input class='input' type='radio' name={props.id} value={0} checked={true}/>
         <span class='checkmark' />
       </label>
       <label class='container'>
-        <input class='input' type='radio' name={props.id} onChange$={() => props.valueSignal.value = 2}/>
+        <input class='input' type='radio' name={props.id} value={1} />
         <span class='checkmark' />
       </label>
-      
+      <label class='container'>
+        <input class='input' type='radio' name={props.id} value={2} />
+        <span class='checkmark' />
+      </label>
+      <label class='container'>
+        <input class='input' type='radio' name={props.id} value={3} />
+        <span class='checkmark' />
+      </label>
     </div>
-    
   );
 });
