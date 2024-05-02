e2873147ee8222f00ef238a8aa75f8437c69a29d
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-05-02 21:40:04 +0200

    feat: :boom: make api request work case specific

--- web/src/routes/test/[id]/index.tsx
+++ web/src/routes/test/[id]/index.tsx
@@ -3,16 +3,27 @@ import { Scale } from '~/components/scale/scale';
 import questionStyles from './question.css?inline'
 import { Form, routeAction$, routeLoader$, useNavigate } from '@builder.io/qwik-city';
 
-export const useSkillsData = routeLoader$(async () => {
+export const useSkillsData = routeLoader$(async ({params, query}) => {
+  const x8 :boolean = query.has('x8');
+  const metodyka: string | null = query.get('metodyka')
 
-  const res = await fetch('http://localhost:1339/api/skills?populate[aspekty][populate]=*&pagination[page]=1&pagination[pageSize]=1', {
-    method: "GET",
-    headers: {
-      'Authorization': 'Bearer 4ad0cbfb8a8e769fd7fc949d4eea2de3d61ccbd40245e40fa9b3eac0b43e01ded24d7c70b9dad432d9a8864c285e121f315eb234686358b896574405cc1e4d86ff3fbfda202a70a818025898a68907864376a3e34b67bfb4740ee0f282637551a75ce385c7a7428b08d3d4b911ed7da1cf71cb9537d35d01587a38363a9df133'
-    }
+
+
+  const res = await fetch(
+    ('http://localhost:1339/api/skills?' +
+    'filters[x8][$eq]=' + x8 +
+    '&filters[$or][0][metodyka][$eq]=N/D' + '&filters[$or][1][metodyka][$eq]=' + metodyka +
+    '&populate[aspekty][populate]=*' +
+    '&pagination[page]=' + params.id +
+    '&pagination[pageSize]=1'), 
+    {
+      method: "GET",
+      headers: {
+        'Authorization': 'Bearer 4ad0cbfb8a8e769fd7fc949d4eea2de3d61ccbd40245e40fa9b3eac0b43e01ded24d7c70b9dad432d9a8864c285e121f315eb234686358b896574405cc1e4d86ff3fbfda202a70a818025898a68907864376a3e34b67bfb4740ee0f282637551a75ce385c7a7428b08d3d4b911ed7da1cf71cb9537d35d01587a38363a9df133'
+      }
   });
-  const aspects = (await res.json());
-  return aspects;
+  const data = (await res.json());
+  return data;
 })
 
 export const useFillQuestionare = routeAction$( async (answ) => {
@@ -24,16 +35,20 @@ export const useFillQuestionare = routeAction$( async (answ) => {
 export default component$(() => {
   useStylesScoped$(questionStyles)
   
-  const data = useSkillsData();
+  let data = useSkillsData();
+  const meta = data.value.meta;
+  data = data.value.data;
   
   const action = useFillQuestionare();
   
   const newTab = useNavigate();
 
+  
+
   return (
     <>
     <Form action={action}>  
-    {data.value.data.map(({attributes, id}) => (
+    {data.map(({attributes, id}) => (
       <>
         {attributes.aspekty.map(({definicje, id}) => (
           <>
