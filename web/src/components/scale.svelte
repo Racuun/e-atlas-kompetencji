c88cb77b8dbca00731044b35d268db94101d63ca
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-10-10 15:27:39 +0200

    style: :lipstick: create Scale component

---
+++ web/src/components/scale.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let agreement: number = 3;
+</script>
+
+<div class ="scale_div">
+    <input type=radio bind:group={agreement} value={0}/>
+    <input type=radio bind:group={agreement} value={1}/>
+    <input type=radio bind:group={agreement} value={2}/>
+    <input type=radio bind:group={agreement} value={3}/>
+    <input type=radio bind:group={agreement} value={4}/>
+    <input type=radio bind:group={agreement} value={5}/>
+    <input type=radio bind:group={agreement} value={6}/>
+</div>
+
+<style>
+    .scale_div {
+    }
+</style>
