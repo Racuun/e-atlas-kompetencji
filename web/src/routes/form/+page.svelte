b14880d8a33a933ea3081e2d11b8cd7faf4850ea
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-10-13 19:00:09 +0200

    feat: :sparkles: create form page

---
+++ web/src/routes/form/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import Scale from "../../components/scale.svelte";
+    import type { PageData } from './$types';
+
+
+
+    export let data: PageData;
+</script>
+
+<form>
+    {#each data.retData as answ}
+        <p>{answ.description}</p>
+        <Scale bind:agreement={answ.value}/>
+    {/each}
+
+    {#if pageID == 0}
+        <button type=submit>Finish</button>
+    {/if}
+</form>
+
+<style>
+
+</style>
