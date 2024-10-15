f6b2399863b229d817a9ad4d6a0384d5f6ff0406
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-10-15 15:12:29 +0200

    style: :lipstick: update scale to work with form

--- web/src/components/scale.svelte
+++ web/src/components/scale.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     export let agreement: number = 3;
+
+    export let ID: string = '';
 </script>
 
 <div class ="scale_div">
-    <input type=radio bind:group={agreement} value={0}/>
-    <input type=radio bind:group={agreement} value={1}/>
-    <input type=radio bind:group={agreement} value={2}/>
-    <input type=radio bind:group={agreement} value={3}/>
-    <input type=radio bind:group={agreement} value={4}/>
-    <input type=radio bind:group={agreement} value={5}/>
-    <input type=radio bind:group={agreement} value={6}/>
+    <input name={ID} type=radio bind:group={agreement} value={0}/>
+    <input name={ID} type=radio bind:group={agreement} value={2}/>
+    <input name={ID} type=radio bind:group={agreement} value={1}/>
+    <input name={ID} type=radio bind:group={agreement} value={3}/>
+    <input name={ID} type=radio bind:group={agreement} value={4}/>
+    <input name={ID} type=radio bind:group={agreement} value={5}/>
+    <input name={ID} type=radio bind:group={agreement} value={6}/>
 </div>
 
 <style>
