7562a28dde653027c8201a2f101885c17949822b
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-10-17 17:58:39 +0200

    build: :card_file_box: enable cors in database

--- database/keystone.ts
+++ database/keystone.ts
@@ -14,6 +14,7 @@ import { lists } from './schema'
 // when you write your list-level access control functions, as they typically rely on session data
 import { withAuth, session } from './auth'
 
+
 export default withAuth(
   config({
     db: {
@@ -25,5 +26,8 @@ export default withAuth(
     },
     lists,
     session,
+    server: {
+      cors: { origin: ['http://localhost:5173'], credentials: true },
+    }
   })
 )
