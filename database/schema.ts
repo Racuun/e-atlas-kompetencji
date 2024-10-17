3dbf0fa68be5ff4c2ebeb027363cc92aa74da76b
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-10-17 19:51:24 +0200

    chore: :card_file_box: configure lists display and graphql settings

--- database/schema.ts
+++ database/schema.ts
@@ -51,10 +51,6 @@ export const lists = {
 
       password: password({ validation: { isRequired: true } }),
 
-      // we can use this field to see what Posts this User has authored
-      //   more on that in the Post list below
-      posts: relationship({ ref: 'Post.author', many: true }),
-
       createdAt: timestamp({
         // this sets the timestamp to Date.now() when the user is first created
         defaultValue: { kind: 'now' },
@@ -62,96 +58,9 @@ export const lists = {
     },
   }),
 
-  Post: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
-    access: allowAll,
-
-    // this is the fields for our Post list
-    fields: {
-      title: text({ validation: { isRequired: true } }),
-
-      // the document field can be used for making rich editable content
-      //   you can find out more at https://keystonejs.com/docs/guides/document-fields
-      content: document({
-        formatting: true,
-        layouts: [
-          [1, 1],
-          [1, 1, 1],
-          [2, 1],
-          [1, 2],
-          [1, 2, 1],
-        ],
-        links: true,
-        dividers: true,
-      }),
-
-      // with this field, you can set a User as the author for a Post
-      author: relationship({
-        // we could have used 'User', but then the relationship would only be 1-way
-        ref: 'User.posts',
-
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name', 'email'],
-          inlineEdit: { fields: ['name', 'email'] },
-          linkToItem: true,
-          inlineConnect: true,
-        },
-
-        // a Post can only have one author
-        //   this is the default, but we show it here for verbosity
-        many: false,
-      }),
-
-      // with this field, you can add some Tags to Posts
-      tags: relationship({
-        // we could have used 'Tag', but then the relationship would only be 1-way
-        ref: 'Tag.posts',
-
-        // a Post can have many Tags, not just one
-        many: true,
-
-        // this is some customisations for changing how this will look in the AdminUI
-        ui: {
-          displayMode: 'cards',
-          cardFields: ['name'],
-          inlineEdit: { fields: ['name'] },
-          linkToItem: true,
-          inlineConnect: true,
-          inlineCreate: { fields: ['name'] },
-        },
-      }),
-    },
-  }),
-
-  // this last list is our Tag list, it only has a name field for now
-  Tag: list({
-    // WARNING
-    //   for this starter project, anyone can create, query, update and delete anything
-    //   if you want to prevent random people on the internet from accessing your data,
-    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
-    access: allowAll,
-
-    // setting this to isHidden for the user interface prevents this list being visible in the Admin UI
-    ui: {
-      isHidden: true,
-    },
-
-    // this is the fields for our Tag list
-    fields: {
-      name: text(),
-      // this can be helpful to find out all the Posts associated with a Tag
-      posts: relationship({ ref: 'Post.tags', many: true }),
-    },
-  }),
-
 
   Definicja: list({
-      access: allowAll,
+    access: allowAll,
 
     fields: {
       opis: text(),
@@ -162,6 +71,22 @@ export const lists = {
         ref: 'Aspekt',
         many: false,
       })
+    },
+
+    ui: {
+      label: 'Definicje',
+      singular: 'Definicja',
+      plural: 'Definicje'
+    },
+
+    graphql: {
+      plural: 'Definicje',
+      omit: {
+        query: false,
+        create: true,
+        update: true,
+        delete: true,
+      }
     }
   }),
 
@@ -178,11 +103,36 @@ export const lists = {
           displayMode: 'select',
 
         }
+      }),
+      kompetencja: relationship({
+        ref: 'Kompetencja',
+        many: false,
+        ui: {
+          hideCreate: false,
+          displayMode: 'select',
+
+        }
       })
+    },
+
+    ui: {
+      label: 'Aspekty',
+      singular: 'Aspekt',
+      plural: 'Aspekty'
+    },
+
+    graphql: {
+      plural: 'Aspekty',
+      omit: {
+        query: false,
+        create: true,
+        update: true,
+        delete: true,
+      }
     }
   }),
 
-  Kompetecja: list({
+  Kompetencja: list({
     access: allowAll,
 
     fields: {
@@ -207,6 +157,22 @@ export const lists = {
 
         }
       })
+    },
+
+    ui: {
+      label: 'Kompetencje',
+      singular: 'Kompetencja',
+      plural: 'Kompetencje'
+    },
+
+    graphql: {
+      plural: 'Kompetencje',
+      omit: {
+        query: false,
+        create: true,
+        update: true,
+        delete: true,
+      }
     }
   }),
 
