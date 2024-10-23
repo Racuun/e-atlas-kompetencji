c3951aa2cdf386c0eb414812b7791d71a82e6ee7
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-10-23 14:27:01 +0200

    feat: :sparkles: working analyze algo

--- web/src/components/scale.svelte
+++ web/src/components/scale.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-    export let agreement: number = 3;
+    export let agreement: number = 0;
 
     export let ID: string = '';
 </script>
 
 <div class ="scale_div">
+    <input name={ID} type=radio bind:group={agreement} value={-3}/>
+    <input name={ID} type=radio bind:group={agreement} value={-2}/>
+    <input name={ID} type=radio bind:group={agreement} value={-1}/>
     <input name={ID} type=radio bind:group={agreement} value={0}/>
-    <input name={ID} type=radio bind:group={agreement} value={2}/>
     <input name={ID} type=radio bind:group={agreement} value={1}/>
+    <input name={ID} type=radio bind:group={agreement} value={2}/>
     <input name={ID} type=radio bind:group={agreement} value={3}/>
-    <input name={ID} type=radio bind:group={agreement} value={4}/>
-    <input name={ID} type=radio bind:group={agreement} value={5}/>
-    <input name={ID} type=radio bind:group={agreement} value={6}/>
 </div>
 
 <style>
