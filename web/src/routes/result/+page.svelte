6e02d9523fc5a05bc558c64acf038e54f791100a
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2024-11-26 16:32:22 +0100

    feat: :sparkles: make result page display users result

--- web/src/routes/result/+page.svelte
+++ web/src/routes/result/+page.svelte
@@ -2,19 +2,13 @@
   import { Chart } from "chart.js/auto";
   import type { PageData } from "../$types";
   import { onMount } from "svelte";
+  import Bar from "../../components/bar.svelte";
 
     let chart: any;
     onMount(() => {
         new Chart(chart.getContext('2d'), {
             type: 'radar',
-            data: {
-            labels: ['1','2','3','4','5','6','7','8'],
-            datasets: [{
-                label: '# of Votes',
-                data: [7.0, 9.0, 3, 5, 2, 3, 4.0, 5.5],
-                borderWidth: 3
-            }]
-            },
+            data: chartData,
             options: {
                 scales: {
                     r: {
@@ -31,6 +25,18 @@
 
 
     export let data: PageData;
+
+    let chartData = {
+        labels: Object.values(data.kom).map((value) => {return value.name as string}),
+        datasets: [{
+            label: 'Twój poziom kompetencji',
+            data: Object.keys(data.kom).map((key) => {return data.KomData[key].value as number}),
+            borderWidth: 3
+        }]
+    }
+
+    console.log(JSON.stringify(chartData))
+
 </script>
 
 <div class="row">
@@ -40,10 +46,10 @@
 
     <div class="column">
         <table>
-            {#each Object.keys(data.KomData) as  key }
+            {#each Object.keys(data.AspData) as  key }
             <tr>
                 <td>{key}</td>
-                <td>{data.KomData[key].value}</td>
+                <td><Bar max={9.0} value={parseFloat(data.AspData[key].value)} name={data.asp[key].name}></Bar></td>
             </tr>
             {/each}
         </table>
