50645bb85306fd2e253dc550854c73e16a8c182c
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2025-02-22 02:23:55 +0100

    style: :sparkles: introduce test config on landing page

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
+  import RadioButton from "$lib/components/radio_button.svelte";
 
 
+    let metodyka = $state('');
+    let typ = $state('');
+
+
+
+    export const prerender = true;
 </script>
 
 
@@ -12,25 +19,40 @@
 
 <div class="back">
     <img class="map" src="map.jpg" alt="Map not found"/>
-    <div class="octagon"/>
 </div>
 
+<main>
 
-<div class="main">
-    <div class="hero">
+    <section class="hero">
         <img src="roza-wiatrow.png" alt="" class="roza"/>
         <div class="title">
             <h1>Atlas</h1>
             <h2>KOMPETENCJI</h2>
         </div>
 
-        <button class="to-quest" on:click={ async(e) => {goto("/quest")}}>Sprawdź się</button>
+    </section>
 
-    </div>
-    <div class="bottom-shape" >
+    <section id="test">
+        <section id="type">
+            <h3>Format</h3>
+            <RadioButton radioName="typ" bind:group={typ} label="8K" value='8k' />
+            <RadioButton radioName="typ" bind:group={typ} label="5K" value='5k' />
+        </section>
 
-    </div>
-</div>
+        <section id="met">
+            <h3>Metodyka</h3>
+            <RadioButton radioName="metodyka" bind:group={metodyka} label="Harcerska" value='harc' />
+            <RadioButton radioName="metodyka" bind:group={metodyka} label="Zuchowa" value='zuch' />
+            <RadioButton radioName="metodyka" bind:group={metodyka} label="Wędrownicza" value='wedr' />
+        </section>
+
+        <button class="to-quest" onclick={ async(e) => {goto("/extended")}} disabled={!(metodyka!=='' && typ!=='')}>Rozpocznij test</button>
+
+    </section>
+
+</main>
+
+
 
 <div class="header">
     <div class=logo-circle>
@@ -41,7 +63,12 @@
 
 <style>
     .back{
-        background-color: #DCDCDD;
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        background: #DCDCDD;
+        width: 100vw;
+        height: 100vh;
     }
     .map {
         position: fixed;
@@ -53,7 +80,8 @@
         min-height: 100vh;
         transform: translate(-45.5%, 0);
         overflow: hidden;
-        opacity: 0.6;
+        opacity: 0.8;
+        filter: invert(0.1);
     }
     /* .octagon {
       width: 100px;
@@ -86,12 +114,16 @@
     } */
 
 
-    .main {
+    main {
         position: absolute;
         left: 0px;
         top: 0px;
         width: 100%;
-        height: 100%;
+        height: 100vh;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        align-items: center;
+
     }
 
     .header{
@@ -107,6 +139,7 @@
         left: -30px;
         position: relative;
         background: #717D64;
+        opacity: 1;
         border-radius: 0% 82% 60% 140% / 88% 97% 80% 60% ;
     }
     .logo-zhr {
@@ -121,7 +154,6 @@
 
     .hero {
         display: flex;
-        margin-top: 50px;
         height: fit-content;
         width: 100%;
         flex-flow: column;
@@ -136,6 +168,7 @@
     .title {
         width: fit-content;
         font-family: 'IMEnglish';
+        color: #1F1F2E;
     }
     h1{
         margin: 0;
@@ -171,26 +204,54 @@
         font-size: 20px;
         text-transform: uppercase;
     }
-    .to-quest:hover {
+    .to-quest:hover:active {
         background-color: #717D64;
         color: #DCDCDD;
         transition-duration: 350ms;
     }
+    .to-quest:disabled,
+    .to-quest:disabled:hover {
+        border-color: #DCDCDD;
+        color: grey;
+    }
 
-
-    .bottom-shape {
-        --c: 60; /* control the curvature (a percentage value without unit bigger than 50%)*/
-
+    #test {
         width: 100%;
-        height: 50px;
-        transform: translate(-50%);
-        position: absolute;
-        bottom: 0px;
-        left: 50%;
-        aspect-ratio: 3/2;
-        background: #717D64;
-        mask: radial-gradient(calc(var(--c)*1%) 100% at 50% calc(100% + 100%*cos(asin(50/var(--c)))),#0000 calc(100% - 1px),#000);
-        clip-path: ellipse(calc(var(--c)*1%) 100% at bottom);
+        align-items: center;
+
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        gap: 20px;
+
+        padding-top: 20%;
+        padding-bottom: 20%;
+        border-radius: 50px 0px 0px 50px;
+        background: #DCDCDD;
+        box-shadow: 0 0px 20px 10px #DCDCDD;
+    }
+    #type {
+        display: flex;
+        width: 90%;
+        grid-template-columns: 1fr 1fr;
+        gap: 10px   ;
+        justify-content: center;
+        align-content: center;
+    }
+    #met {
+        display: grid;
+        width: 90%;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 10px   ;
+        justify-content: center;
+        align-content: center;
+    }
+    #met h3 {
+        grid-column: 1 / 4;
+        text-align: center;
+        font-size: 26px;
+        font-family: 'IMEnglish';
+        font-weight: 900;
     }
 
 </style>
