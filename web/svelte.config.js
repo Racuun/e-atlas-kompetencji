fae5a740ed8818c0d5686e0d0360e2f2b267539d
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2025-02-24 00:20:45 +0100

    fix: :green_heart: fix svelte config

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -8,11 +8,10 @@ const config = {
 	preprocess: vitePreprocess({ script: true }),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		checkOrigin: false,
+		csrf: {
+			checkOrigin: false,
+		},
 	}
 };
 
