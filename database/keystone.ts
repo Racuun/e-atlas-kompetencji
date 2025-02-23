a066fd352d58fa9db33dd14ba00162e6e9ef9f3b
Raccoon <lukaszkmiecikdodtkowe@gmail.com>
2025-02-23 22:00:36 +0100

    fix: :pencil2: remove typos in keystone.ts

--- database/keystone.ts
+++ database/keystone.ts
@@ -3,7 +3,6 @@ import 'dotenv/config'
 import { config } from '@keystone-6/core'
 import { lists } from './schema'
 import { withAuth, session } from './auth'
-import {onHeaders} from 'on-headers'
 
 export default withAuth(
   config({
@@ -21,16 +20,6 @@ export default withAuth(
       port: parseInt(process.env.PORT as string),
       cors: { origin: ['http://web:3000', 'http://localhost:5137'], credentials: true },
       extendExpressApp: (app) => {
-        // app.use((req, res, next) => {
-        //   onHeaders(res, () => {
-        //     // Should be an array; let's join it together
-        //     if (res.getHeader('set-cookie') == undefined) return;
-        //     const headerValue = Array.isArray(res.getHeader('set-cookie')) ? res.getHeader('set-cookie').join(' ') : ''
-        //     console.log('Set-Cookie response header being set as...\nSet-Cookie: ', headerValue)
-        //   });
-        //   next();
-        // });
-
         app.set('trust proxy', true);
       },
     },
@@ -41,7 +30,7 @@ export default withAuth(
         type: 'image',
         generateUrl: path => `http://localhost:${process.env.PORT as string}/images${path}`,
         serverRoute: {
-          path: '/images'
+          path: '/cms/images'
         },
         storagePath: 'public/images'
       }
